43c18b50c57d920162ddbd45886ac9971076bb2a
Jordan Andrews <jordandrews@hotmail.com>
2024-01-30 22:38:00 +1100

    WIP

--- src/probe/variables.rs
+++ src/probe/variables.rs
@@ -1,6 +1,8 @@
+use reqwest::get;
 use serde_json::Value;
-use std::collections::HashMap;
+use std::{collections::HashMap, env::var};
 use regex::Regex;
+use lazy_static::lazy_static;
 
 pub struct StoryVariables {
     pub step_variables: HashMap<String, StepVariables>
@@ -10,9 +12,12 @@ pub struct StepVariables {
     pub response_body: String
 }
 
-fn dynamic_substitute_variables(content: &str, variables: &StoryVariables) -> String {
-    let re = Regex::new(r"\$\{\{(.*?)\}\}").unwrap();
-    re.replace_all(content, |caps: &regex::Captures| {
+lazy_static! {
+    static ref SUB_REGEX: Regex = Regex::new(r"\$\{\{(.*?)\}\}").unwrap();
+}
+
+fn substitute_variables(content: &str, variables: &StoryVariables) -> String {
+    SUB_REGEX.replace_all(content, |caps: &regex::Captures| {
         let placeholder = &caps[1];
         let parts: Vec<&str> = placeholder.split('.').collect();
 
@@ -25,33 +30,43 @@ fn dynamic_substitute_variables(content: &str, variables: &StoryVariables) -> St
 }
 
 fn get_generated_value(type_to_generate: Option<&&str>) -> String {
-    unimplemented!()
+
+    // handle option
+
+    match type_to_generate {
+        "uuid" => substitute_step_value(&parts[1..], variables),
+        _ => "".to_string(),
+    }
 }
 
 fn substitute_step_value(parts: &[&str], variables: &StoryVariables) -> String {
     
-    // Use the first index to find the right step
-    let key = parts.join(".");
+    let step_name = parts[0];
 
+    let step = &variables.step_variables[step_name]; // todo test non existent variable name
 
-    // If needed do the finnicky json thing - I think the old code was better for that
-    steps_data.get(&key)
-        .map(|value| {
-            if parts.len() > 1 {
-                value.get(parts.last().unwrap()).map_or("".to_string(), |v| v.to_string())
-            } else {
-                value.to_string()
-            }
-        })
-        .unwrap_or_default()
+    // Technically we should check the types of these middle 2 values "response.body"
+
+    if parts.len() > 3 {
+        return get_nested_json_value(&parts[3..], &step.response_body);
+    } else {
+        return step.response_body.clone()
+    }
 }
+
+fn get_nested_json_value(parts: &[&str], json_string: &String) -> String {
+    unimplemented!()
+}
+
+// todo test non existent variable name
+// also test if there aren't enough "." so we go out of index
 
 #[tokio::test]
 async fn test_dynamic_substitute_variables() {
-    let yaml_content = r#"
+    let content = r#"
     entire_body: ${{steps.get-token.response.body}}
-    token: "${{steps.get-token.response.body.token}}"
-    uuid: "${{generate.uuid}}"
+    token: ${{steps.get-token.response.body.token}}
+    uuid: "${{generate.uuid}}
     "#;
 
     // Simulate a response body
@@ -59,17 +74,17 @@ async fn test_dynamic_substitute_variables() {
         "token": "12345",
         "other_field": "value"
     }"#;
-    let body: serde_json::Value = serde_json::from_str(body_str).unwrap();
 
-    // Simulate steps data
-    let steps_data = HashMap::from([
-        ("get-token.response.body".to_string(), body),
-    ]);
+    // Simulate variables from story
+    let variables = StoryVariables {
+        step_variables: HashMap::from([
+            ("get-token".to_string(), StepVariables{
+                response_body: body_str.to_string()
+            })
+        ])
+    };
 
-    // Simulate UUID generation (for testing, using a fixed value)
-    let uuid = "generated-uuid-1234";
-
-    let result = dynamic_substitute_variables(yaml_content, &steps_data, uuid);
+    let result = substitute_variables(content, &variables);
 
     let expected_result = r#"
     entire_body: {"token":"12345","other_field":"value"}
