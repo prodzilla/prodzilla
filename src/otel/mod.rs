e98c20de5419db18492db547e9f3f0260765c588
David Stevens <davidstevens.nz@gmail.com>
2024-05-03 14:57:14 +0200

    chore: move otel resource init to module root

--- src/otel/mod.rs
+++ src/otel/mod.rs
@@ -1,1 +1,13 @@
+use std::time::Duration;
+
+use opentelemetry_sdk::{
+    resource::{EnvResourceDetector, ResourceDetector},
+    Resource,
+};
+
+pub(crate) mod metrics;
 pub(crate) mod tracing;
+
+pub fn resource() -> Resource {
+    Resource::default().merge(&EnvResourceDetector::new().detect(Duration::from_secs(3)))
+}
--- src/otel/tracing.rs
+++ src/otel/tracing.rs
@@ -1,26 +1,17 @@
-use std::time::Duration;
-
 use opentelemetry::global;
 use opentelemetry_sdk::propagation::TraceContextPropagator;
-use opentelemetry_sdk::resource::{EnvResourceDetector, ResourceDetector};
-use opentelemetry_sdk::{runtime, Resource};
+use opentelemetry_sdk::runtime;
 use opentelemetry_sdk::trace::{BatchConfig, Tracer};
 
-pub fn create_tracer() -> Tracer{
+use super::resource;
+
+pub fn create_tracer() -> Tracer {
     global::set_text_map_propagator(TraceContextPropagator::new());
     opentelemetry_otlp::new_pipeline()
         .tracing()
-        .with_trace_config(
-            opentelemetry_sdk::trace::Config::default()
-                .with_resource(
-                    Resource::default().merge(
-                        &EnvResourceDetector::new().detect(Duration::from_secs(3))
-                    )
-                )
-        )
+        .with_trace_config(opentelemetry_sdk::trace::Config::default().with_resource(resource()))
         .with_batch_config(BatchConfig::default())
         .with_exporter(opentelemetry_otlp::new_exporter().tonic())
         .install_batch(runtime::Tokio)
         .unwrap()
 }
-
