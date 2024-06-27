4cf2edf389b78fac5456fa8254a04d8d2596534b
David Stevens <github@davidstevens.se>
2024-06-27 10:44:11 +0200

    chore: Remove unused import

--- src/probe/probe_logic.rs
+++ src/probe/probe_logic.rs
@@ -2,7 +2,6 @@ use std::sync::Arc;
 
 use chrono::Utc;
 use opentelemetry::global;
-use opentelemetry::global::ObjectSafeSpan;
 use opentelemetry::trace;
 use opentelemetry::trace::FutureExt;
 use opentelemetry::trace::Status;
